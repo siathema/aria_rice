b8e3f354bbdefffe83b1863e3e0a2cc480f4097c
Aria Linford <nate.holaday@gmail.com>
2024-04-04 12:37:21 -0700

    Added some xrandr configs.

--- setup_arch.sh
+++ setup_arch.sh
@@ -4,7 +4,7 @@ echo "Hey there, if you're not using arch this isn't for you. Also we're gonna d
 read -n 2 -p "Do you really want to do this?(Y/n): " answ
 if [[ $answ =~ [yY]{1} ]]; then
     echo "Installing packages..."
-    sudo pacman --noconfirm -S i3-wm i3status dmenu unclutter xwallpaper xcompmgr sxhkd dunst alacritty stow pulsemixer
+    sudo pacman --noconfirm -S i3-wm i3status dmenu unclutter xwallpaper xorg-xinit xcompmgr sxhkd dunst alacritty stow pulsemixer
     echo "done."
     echo "Removing old configs..."
     rm -fr ~/.bash* ~/.profile ~/.config/nvim ~/.configs/sxhkd ~/.configs/i3* ~/.configs/alacritty ~/.x* ~/.Xresources
